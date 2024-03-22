ecc52b2748752d2eb7d517a82b34f077ea548ee1
Felipe Rosas <contato@eufelipe.com>
2024-03-22 18:20:26 -0300

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Log Finance App",
+  description: "App to log daily earnings and expenses",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
