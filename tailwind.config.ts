14a4deeeac26490eb5641580eff14f9f9d04758e
Felipe Rosas <contato@eufelipe.com>
2024-03-27 09:48:16 -0300

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,13 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/presentation/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        "primary-dark": "#05151e",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
