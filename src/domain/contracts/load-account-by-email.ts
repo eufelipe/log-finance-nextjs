c5e23184c75261510e20c145f933ee5b6f3e59f1
Felipe Rosas <contato@eufelipe.com>
2024-03-28 09:54:22 -0300

    chore: add load account by email contract

---
+++ src/domain/contracts/load-account-by-email.ts
@@ -0,0 +1,13 @@
+import { Account } from "@/domain/models";
+
+type Input = LoadAccountByEmailRepository.Input;
+type Output = LoadAccountByEmailRepository.Output;
+
+export interface LoadAccountByEmailRepository {
+  loadByEmail: (input: Input) => Promise<Output>;
+}
+
+export namespace LoadAccountByEmailRepository {
+  export type Input = string;
+  export type Output = Account;
+}
