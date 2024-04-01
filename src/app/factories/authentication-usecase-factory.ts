4b85accec19e0e2a50bf881f14d7bd45b314e123
Felipe Rosas <contato@eufelipe.com>
2024-03-31 22:52:43 -0300

    chore: add authentication usecase factory

---
+++ src/app/factories/authentication-usecase-factory.ts
@@ -0,0 +1,19 @@
+import { AuthenticationUseCase } from "@/domain/usecases";
+
+import { AccountPostgreSqlRepository, prismaClient } from "@/infra";
+
+import { EmailValidation } from "@/validation/validators";
+
+import { createAccountUseCaseFactory } from "./create-account-usecase-factory";
+
+const accountPostgreSqlRepository = new AccountPostgreSqlRepository(
+  prismaClient,
+);
+
+const emailValidation = new EmailValidation("email");
+
+export const authenticationUseCaseFactory = new AuthenticationUseCase(
+  accountPostgreSqlRepository,
+  createAccountUseCaseFactory,
+  emailValidation,
+);
