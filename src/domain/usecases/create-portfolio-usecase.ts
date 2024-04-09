a3c861f6864a5ca5020d9914f0b8544567712973
Felipe Rosas <contato@eufelipe.com>
2024-04-09 08:01:37 -0300

    chore: add validation on create portfolio use case

--- src/domain/usecases/create-portfolio-usecase.ts
+++ src/domain/usecases/create-portfolio-usecase.ts
@@ -1,19 +1,32 @@
 import { AddPortfolioRepository } from "@/domain/contracts/repositories";
 import { CreatePortfolio } from "@/domain/entities";
 import { UnexpectedError } from "@/domain/errors";
+import { Validation } from "@/validation/contracts";
+import { RequiredFieldError, ValidationError } from "@/validation/errors";
 
 type Input = CreatePortfolio.Input;
 type Output = CreatePortfolio.Output;
 
 export class CreatePortfolioUseCase implements CreatePortfolio {
-  constructor(private readonly repository: AddPortfolioRepository) {}
+  constructor(
+    private readonly repository: AddPortfolioRepository,
+    private readonly validation: Validation,
+  ) {}
 
   async create(input: Input): Promise<Output> {
     try {
+      const { name } = input;
+
+      this.validation.validate(name);
+
       const account = await this.repository.add(input);
 
       return account;
     } catch (error) {
+      if (error instanceof ValidationError) {
+        throw new RequiredFieldError("Invalid name field");
+      }
+
       throw new UnexpectedError("Error on CreatePortfolio");
     }
   }
