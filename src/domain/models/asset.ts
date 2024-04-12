a54e22bb6af814d45ec80e436421b4178e940aab
Felipe Rosas <contato@eufelipe.com>
2024-04-12 18:02:55 -0300

    chore: update asset model

--- src/domain/models/asset.ts
+++ src/domain/models/asset.ts
@@ -1,13 +1,24 @@
-export type TAssetType =
+export type AssetType =
   | "STOCK"
   | "CRYPTOCURRENCY"
   | "ETF"
-  | "REAL_ESTATE_FUND";
+  | "REAL_ESTATE_FUND"
+  | "FIXED_INCOME"
+  | "COMMODITY"
+  | "MUTUAL_FUND"
+  | "OPTIONS"
+  | "FUTURES"
+  | "SWAPS"
+  | "PRIVATE_DEBT"
+  | "FOREX"
+  | "PRIVATE_EQUITY"
+  | "VENTURE_CAPITAL"
+  | "DIRECT_REAL_ESTATE";
 
 export type Asset = {
   id: string;
   name: string;
-  assetType: TAssetType;
+  assetType: AssetType;
   description?: string;
 };
 
